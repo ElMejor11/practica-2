a8a6491c860b6a70aa3b713b13ddb5c8b206cffb
ElMejor11 <158163759+ElMejor11@users.noreply.github.com>
2024-02-07 12:50:40 +0100

    Create practicaGestionDatosR.R

---
+++ practicaGestionDatosR.R
@@ -0,0 +1,92 @@
+nuevo_dir <- "C:/practicas"
+setwd(nuevo_dir)
+
+1: "edades_descubrimientos"
+edades <- c(14,16,17,32,43,50)
+valor_suma <- 14+16+17+32+43+50
+Valor_div <- 172/6
+mean(28.6666666666667)
+
+2: "cantidad_artefactos"
+set.seed(123)
+n_registros <- 8
+cantidad_artefactos <- sample(1:142, n_registros, replace = TRUE)
+
+3: "profundidad_hallazgos
+vector <- c (2,4,5,10)
+max(vector)
+
+4: "materiales_encontrados"
+set.seed(123)
+n_registros <- 14
+
+materiales_encontrados <- sample(c("collar","espada","restos_oseos"), n_registros, replace = TRUE)
+
+5: "años_excavaciones"
+set.seed(123)
+n_registros <- 20
+yacimiento<-sample(c("necrópolis_carmona", "Valencina", "Antiquarium"), n_registros, replace = TRUE)
+años_excavaciones <- sample(1:20, n_registros, replace = TRUE)
+
+6: matriz
+
+n_registros <- 20
+yacimiento<-sample(c("necrópolis_carmona", "Valencina", "Antiquarium"), n_registros, replace = TRUE)
+vector <- c (2,4,5,10)
+matriz1 <- matrix(c(vector),
+                  nrow = 20, ncol = 3)
+max  (matriz1)
+
+7: matriz 2
+
+vector <- c (5,8,9,15)
+matriz2 <- matrix(c(vector),
+                  nrow = 4, ncol = 3,
+                  byrow = T)
+max (matriz2 (ncol))
+
+8: matriz 3
+
+vector <- c (1,5,6,11)
+matriz3 <- matrix(c(vector),
+                  nrow = 20, ncol = 3)
+
+9: matriz 4
+
+vector <- c (1,5,6,11)
+matriz4 <- matrix(c(vector),
+                  nrow = 20, ncol = 3)
+
+10: matriz 5
+
+
+11: registro_artefactos
+
+n_registros <- 20
+
+data_frame <- data.frame (
+  sitio_arqueologico=c(("montelirio","carambolo","italica"), n_registros, replace = TRUE),
+  tipo_artefacto=c(("hueso","espada", "piedra"), n_registros, replace = TRUE),
+  fecha de descubrimiento=c((2000,2005,2006), n_registros, replace = TRUE),
+  descripcion=c(("dolmen", "templo", "ciudad"), n_registros, replace = TRUE)
+)
+  
+registro_artefactos <- data.frame(
+  sitio_arqueologico=sitio_arqueologico,
+  tipo_artefacto=tipo_artefacto,
+  fecha de descubrimiento=fecha de descubrimiento,
+  descripcion=descripcion
+)
+print(archaeological_data)
+print(data_frame)
+
+12: excavaciones_equipo
+
+
+13: datos_esqueletos
+
+
+14: ubicaciones_geográficas
+
+
+
